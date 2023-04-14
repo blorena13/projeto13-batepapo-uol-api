f00902646c7f6d5e3f0be30edbb7ecb8a3b06eca
Lorena <blorena1321@gmail.com>
2023-04-13 21:31:44 -0300

    requisito limit

--- src/app.js
+++ src/app.js
@@ -16,8 +16,8 @@ const participantes = [];
 let db;
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 mongoClient.connect()
-.then(()=> db = mongoClient.db())
-.catch((err) => console.log(err.message))
+    .then(() => db = mongoClient.db())
+    .catch((err) => console.log(err.message))
 
 app.post("/participants", (req, res) => {
 
@@ -28,7 +28,7 @@ app.post("/participants", (req, res) => {
         name,
         lastStatus
     }
-    
+
 
     const promise = db.collection("participants").insertOne(obj);
     promise.then(() => res.sendStatus(201));
@@ -46,31 +46,48 @@ app.get("/participants", (req, res) => {
 
 app.post("/messages", (req, res) => {
     const { to, text, type } = req.body;
-    const user = req.headers.user;
-    
+    const user = req.headers;
+
     const time = dayjs().format('HH:mm:ss');
 
-    const newMessage = {from: user, to, text, type, time}
+    const newMessage = { from: user, to, text, type, time }
 
     const promise = db.collection("messages").insertOne(newMessage);
-    promise.then(()=> res.sendStatus(201));
-    promise.catch(()=> res.sendStatus(500));
-    
+    promise.then(() => res.sendStatus(201));
+    promise.catch(() => res.sendStatus(500));
+
 })
 
 app.get("/messages", (req, res) => {
-   
-    const { user } = req.headers.user;
+
+    const { user } = req.headers;
     const { type } = req.query;
+    const limit = parseInt(req.query.limit);
 
 
     const promise = db.collection("messages").find({}).toArray();
-    promise.then(message => res.send(message.filter(op => type ? op.type === type : true)));
+    promise.then(
+        message => {
+
+            if ( isNaN(limit) || limit <= 0) {
+                res.sendStatus(422);
+            }else if (limit) {
+                const limitedMessage = message.slice(0, limit);
+                const filteredMessage = limitedMessage.filter(op => type ? op.type === type : true);
+                res.send(filteredMessage);
+            
+            } else {
+                const filteredMessage = message.filter(op => type ? op.type === type : true);
+                res.send(filteredMessage);
+            }
+        });
+
     promise.catch(() => res.sendStatus(500));
 })
 
+
 app.post("/status", (req, res) => {
-    
+
 })
 
 app.listen(PORT, () => console.log(`rodando servidor na porta ${PORT}`));
